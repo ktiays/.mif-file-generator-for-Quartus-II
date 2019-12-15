fcac5aac4caa3a620dfca594e6af3734e7356f47
chenyuxin <cuansky@vip.qq.com>
2019-12-15 18:23:38 +0800

    * 删除部分无用代码

--- src/com/frozen/sakura/Main.java
+++ src/com/frozen/sakura/Main.java
@@ -3,7 +3,6 @@ package com.frozen.sakura;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(args[1]);
 	    FileHelper fileHelper = new FileHelper();
 	    fileHelper.createFile();
     }
